6b56700157a8f3ff5f72f92eb77f5e4f56ccd2c2
Pavlo Morozov <pavlomorozov78@gmail.com>
2018-01-16 18:34:11 +0200

    The onClick event handler not called
    if breakpoint set in ngAfterViewChecked()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  host: {
+    '(document:touchstart)': 'onTouchStart($event)'
+  }
 })
 export class AppComponent {
   title = 'app';
+  flagCollapse = true;
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  onClick(){
+    console.log("onClick");
+  }
+
+  @HostListener('document:touchstart', ['$event'])
+  onTouchStart(ev) {
+    console.log("HostListener document:touchstart");
+  }
 }
